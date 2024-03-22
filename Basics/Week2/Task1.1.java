13b0d25af939b3eb60a09f71fb78c2c99ccb38a1
Nader Mamdouh <nadermamdouh20@gmail.com>
2024-03-22 20:55:01 +0200

    Create Task1.1.java

---
+++ Basics/Week2/Task1.1.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args)
+    {
+       /*Variables and Data Types: Declare variables of different data types (int, double, boolean, String) to store the following information:
+        Age of a person
+        Height of a building
+        Whether it's raining (true/false)
+        Name of a city
+        */
+        int age=20;
+        double height=20;
+        boolean is_raining=false;
+        String city="Giza";
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(is_raining);
+        System.out.println(city);
+    }
+}
