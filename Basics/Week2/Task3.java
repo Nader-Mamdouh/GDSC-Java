3bc296b7f69233d4953f946c5b87f87e5a6e4e97
Nader Mamdouh <nadermamdouh20@gmail.com>
2024-03-22 21:09:25 +0200

    Create Task3.java

---
+++ Basics/Week2/Task3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+       /*Write a program to print all even numbers between 1 and 20 using a for loop.*/
+        for (int i=1;i<=20;i++)
+        {
+            if (i%2==0)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
